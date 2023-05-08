79245fe69f75a4b250b9af64061bf3a049a8c91b
jadugarmjadugar <86535184+jadugarmjadugar@users.noreply.github.com>
2023-05-09 02:15:14 +0530

    Update file_jenkins.groovy

--- vars/file_jenkins.groovy
+++ vars/file_jenkins.groovy
@@ -1,4 +1,4 @@
-def call(Map config = [:]) {
-    loadLinuxScript(name: 'script_test.sh')
-    sh "./script_test.sh ${config.name}"
+def call() {
+    loadLinuxScript(name: 'scripttest.sh')
+    sh "./scripttest.sh"
 }
