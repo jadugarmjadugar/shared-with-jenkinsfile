00beaf7a48c5be72904302fd65125d40232efbbf
jadugarmjadugar <86535184+jadugarmjadugar@users.noreply.github.com>
2023-05-08 15:50:22 +0530

    Update file_jenkins.groovy

--- vars/file_jenkins.groovy
+++ vars/file_jenkins.groovy
@@ -1,3 +1,15 @@
 def call(){
   sh 'echo "hello from shared lib"'
+  pipeline{
+    agent any
+    stages{
+      stage('lib_stage1'){
+        steps{
+          script{
+            echo "hello from shared library"
+          }
+        }
+      }
+    }
+  }
 }
