6e47939a107fdb50f0f2d4ae187807c22b28599d
jadugarmjadugar <86535184+jadugarmjadugar@users.noreply.github.com>
2023-05-10 00:32:11 +0530

    Update script_outside1.sh

--- script_outside1.sh
+++ script_outside1.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 MY_VARIABLE=$1
-echo "hello ${MY_VARIABLE} in the library outside not in resource"
+new_variable=$2
+echo "hello ${MY_VARIABLE} in the library outside not in resource ${new_variable}. Hi i am again variable one ${MY_VARIABLE}"
